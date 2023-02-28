90179ac49f4304522f488fcce54ab2dbdefc94d9
guillaume <guillaume.belanger27@gmail.com>
2023-02-28 11:22:38 -0500

    Allows for using a default configuration which would make the setup much quicker for demos and testing

--- src/charm.py
+++ src/charm.py
@@ -11,9 +11,9 @@ from typing import Optional
 
 from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
 from lightkube.models.core_v1 import ServicePort
-from ops.charm import ActionEvent, CharmBase, PebbleReadyEvent
+from ops.charm import ActionEvent, CharmBase, ConfigChangedEvent, PebbleReadyEvent
 from ops.main import main
-from ops.model import ActiveStatus, BlockedStatus
+from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
 from ops.pebble import Layer
 
 logger = logging.getLogger(__name__)
@@ -29,6 +29,7 @@ class SIMAPPOperatorCharm(CharmBase):
         super().__init__(*args)
         self._container_name = self._service_name = "simapp"
         self._container = self.unit.get_container(self._container_name)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(self.on.configure_network_action, self._on_configure_network_action)
         self.framework.observe(self.on.simapp_pebble_ready, self._on_simapp_pebble_ready)
         self._service_patcher = KubernetesServicePatch(
@@ -39,10 +40,39 @@ class SIMAPPOperatorCharm(CharmBase):
             ],
         )
 
-    def _on_simapp_pebble_ready(self, event: PebbleReadyEvent) -> None:
-        if not self._config_file_is_written:
-            self.unit.status = BlockedStatus("Waiting for config file to be written")
+    def _on_config_changed(self, event: ConfigChangedEvent) -> None:
+        if not self._container.can_connect():
+            self.unit.status = WaitingStatus("Waiting for container to be ready")
+            event.defer()
             return
+        if self._use_default_config:
+            self._write_default_config()
+            self._on_simapp_pebble_ready(event)
+
+    def _on_simapp_pebble_ready(
+        self, event: [PebbleReadyEvent, ConfigChangedEvent, ActionEvent]
+    ) -> None:
+        if not self._config_file_is_written:
+            if self._use_default_config:
+                self.unit.status = WaitingStatus("Waiting for config file to be written")
+            else:
+                self.unit.status = BlockedStatus(
+                    "Use `juju scp` to copy the config file to the unit and run the `configure-network` action"  # noqa: E501
+                )
+            return
+        self._container.add_layer("simapp", self._pebble_layer, combine=True)
+        self._container.replan()
+        self.unit.status = ActiveStatus()
+
+    def _write_default_config(self) -> None:
+        with open("src/files/default_config.yaml", "r") as f:
+            content = f.read()
+        self._container.push(source=content, path=f"{BASE_CONFIG_PATH}/{CONFIG_FILE_NAME}")
+        logger.info("Default config file written")
+
+    @property
+    def _use_default_config(self) -> bool:
+        return bool(self.model.config["use-default-config"])
 
     @property
     def _config_file_is_written(self) -> bool:
@@ -59,9 +89,7 @@ class SIMAPPOperatorCharm(CharmBase):
         if not self._config_file_is_written:
             event.fail(message="Config file is not written")
             return
-        self._container.add_layer("simapp", self._pebble_layer, combine=True)
-        self._container.replan()
-        self.unit.status = ActiveStatus()
+        self._on_simapp_pebble_ready(event)
 
     @property
     def _pebble_layer(self) -> Layer:
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -22,6 +22,19 @@ class TestCharm(unittest.TestCase):
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
+    @patch("ops.model.Container.push")
+    def test_given_use_default_config_when_on_config_changed_then_default_config_is_written(
+        self, patch_push
+    ):
+        self.harness.set_can_connect(container="simapp", val=True)
+
+        self.harness.update_config({"use-default-config": True})
+
+        patch_push.assert_called_with(
+            path="/simapp/config/simapp.yaml",
+            source='configuration:\n  device-groups:\n  - imsis:\n    - "208930100007487"\n    - "208930100007488"\n    - "208930100007489"\n    - "208930100007490"\n    - "208930100007491"\n    - "208930100007492"\n    - "208930100007493"\n    - "208930100007494"\n    - "208930100007495"\n    - "208930100007496"\n    - "208930100007497"\n    - "208930100007498"\n    - "208930100007499"\n    - "208930100007500"\n    ip-domain-expanded:\n      dnn: internet\n      dns-primary: 8.8.8.8\n      mtu: 1460\n      ue-dnn-qos:\n        bitrate-unit: bps\n        dnn-mbr-downlink: 200000000\n        dnn-mbr-uplink: 20000000\n        traffic-class:\n          arp: 6\n          name: platinum\n          pdb: 300\n          pelr: 6\n          qci: 9\n      ue-ip-pool: 172.250.1.0/16\n    ip-domain-name: pool1\n    name: 5g-gnbsim-user-group1\n    site-info: aiab\n  - imsis:\n    - "208930100007501"\n    - "208930100007502"\n    - "208930100007503"\n    - "208930100007504"\n    - "208930100007505"\n    - "208930100007506"\n    - "208930100007507"\n    - "208930100007508"\n    - "208930100007509"\n    - "208930100007510"\n    ip-domain-expanded:\n      dnn: internet\n      dns-primary: 8.8.8.8\n      mtu: 1460\n      ue-dnn-qos:\n        bitrate-unit: bps\n        dnn-mbr-downlink: 400000000\n        dnn-mbr-uplink: 10000000\n        traffic-class:\n          arp: 6\n          name: platinum\n          pdb: 300\n          pelr: 6\n          qci: 8\n      ue-ip-pool: 172.250.1.0/16\n    ip-domain-name: pool2\n    name: 5g-gnbsim-user-group2\n    site-info: aiab2\n  network-slices:\n  - application-filtering-rules:\n    - action: permit\n      endpoint: 0.0.0.0/0\n      priority: 250\n      rule-name: ALLOW-ALL\n    name: default\n    site-device-group:\n    - 5g-gnbsim-user-group1\n    - 5g-gnbsim-user-group2\n    site-info:\n      gNodeBs:\n      - name: aiab-gnb1\n        tac: 1\n      - name: aiab-gnb2\n        tac: 2\n      plmn:\n        mcc: "208"\n        mnc: "93"\n      site-name: aiab\n      upf:\n        upf-name: upf\n        upf-port: 8805\n    slice-id:\n      sd: "010203"\n      sst: 1\n  provision-network-slice: false\n  sub-provision-endpt:\n    addr: webui.my-network.svc.cluster.local\n    port: 5000\n  subscribers:\n  - key: 5122250214c33e723a5dd523fc145fc0\n    op: ""\n    opc: 981d464c7c52eb6e5036234984ad0bcf\n    plmnId: "20893"\n    sequenceNumber: 16f3b3f70fc2\n    ueId-end: "208930100007500"\n    ueId-start: "208930100007487"\n  - key: 5122250214c33e723a5dd523fc145fc0\n    op: ""\n    opc: 981d464c7c52eb6e5036234984ad0bcf\n    plmnId: "20893"\n    sequenceNumber: 16f3b3f70fc2\n    ueId-end: "208930100007599"\n    ueId-start: "208930100007501"\ninfo:\n  description: SIMAPP initial local configuration\n  http-version: 1\n  version: 1.0.0\nlogger:\n  APP:\n    ReportCaller: false\n    debugLevel: info\n',  # noqa: E501
+        )
+
     @patch("ops.model.Container.exists")
     def test_given_config_file_is_not_written_when_configure_network_action_then_event_fails(
         self,
@@ -99,5 +112,8 @@ class TestCharm(unittest.TestCase):
         self.harness.container_pebble_ready(container_name="simapp")
 
         self.assertEqual(
-            self.harness.model.unit.status, BlockedStatus("Waiting for config file to be written")
+            self.harness.model.unit.status,
+            BlockedStatus(
+                "Use `juju scp` to copy the config file to the unit and run the `configure-network` action"
+            ),
         )
